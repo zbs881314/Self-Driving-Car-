f8e07c3de66171c72488781d97976d402abb13f1
bob <shibo.zhou14@gmail.com>
2019-01-25 17:24:25 -0500

    update

---
+++ Track/Behavoural clon.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import keras
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from imgaug import augmenters as iaa
+import cv2
+import pandas as pd
+import ntpath
+import random
+
+datadir = 'track'
+columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names=columns)
+pd.set_option('display.max_colwidth', -1)
+print(data.head())
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail
+
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
+
+print(data.head())
+
+
+num_bins = 25
+samples_per_bin = 400
+hist, bins = np.histogram(data['steering'], num_bins)
+print(bins)
+center = (bins[:-1] + bins[1:]) * 0.5
+print(center)
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+#plt.show()
+
+print('total data:', len(data))
+remove_list = []
+for j in range(num_bins):
+    list_ = []
+    for i in range(len(data['steering'])):
+        if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j+1]:
+            list_.append(i)
+    list_ = shuffle(list_)
+    list_ = list_[samples_per_bin:]
+    remove_list.extend(list_)
+
+print('removed:', len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('remaining:', len(data))
+
+hist, _ = np.histogram(data['steering'], (num_bins))
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+
+
+print(data.iloc[1])
+
+def load_img_steering(datadir, df):
+    image_path = []
+    steering = []
+    for i in range(len(data)):
+        indexed_data = data.iloc[1]
+        center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+        image_path.append(os.path.join(datadir, center.strip()))
+        steering.append(float(indexed_data[3]))
+        image_path.append(os.path.join(datadir, left.strip()))
+        steering.append(float(indexed_data[3]) + 0.15)
+        image_path.append(os.path.join(datadir, right.strip()))
+        steering.append(float(indexed_data[3]) - 0.15)
+    image_paths = np.asarray(image_path)
+    steerings = np.asarray(steering)
+    return image_paths, steerings
+
+image_paths, steerings = load_img_steering(datadir + '/IMG', data)
+
+X_train, X_valid, y_train, y_valid = train_test_split(image_paths, steerings, test_size = 0.2, random_state=6)
+
+print('Training Samples: {}\nValid Samples: {}'.format(len(X_train), len(X_valid)))
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+axes[0].hist(y_train, bins=num_bins, width=0.05, color='blue')
+axes[0].set_title('Training set')
+axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
+axes[1].set_title('Validation set')
+#plt.show()
+
+def zoom(image):
+    zoom = iaa.Affine(scale=(1, 1.3))
+    image = zoom.augment_image(image)
+    return image
+
+image = image_paths[random.randint(0, 1000)]
+original_image = mpimg.imread(image)
+zoomed_image = zoom(original_image)
+
+fig, axs = plt.subplots(1, 2, figsize=(15, 10))
+fig.tight_layout()
+
+axs[0].imshow(original_image)
+axs[0].set_title('Original Image')
+axs[1].imshow(zoomed_image)
+axs[1].set_title('Zoomed Image')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
