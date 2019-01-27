e9fab886e6bfe4064f93860c3788b17a38b22c5a
bob <shibo.zhou14@gmail.com>
2019-01-27 17:08:31 -0500

    update

--- Track/Behavoural clon.py
+++ Track/Behavoural clon.py
@@ -119,19 +119,21 @@ print(X_train.shape)
 
 def nvidia_model():
     model = Sequential()
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='relu'))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='elu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='elu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='elu'))
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
     model.add(Dropout(0.5))
 
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
+    model.add(Dense(100, activation='elu'))
     model.add(Dropout(0.5))
 
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(50, activation='elu'))
+
+    model.add(Dense(10, activation='elu'))
+
     model.add(Dense(1))
 
     optimizer = Adam(lr=1e-3)
